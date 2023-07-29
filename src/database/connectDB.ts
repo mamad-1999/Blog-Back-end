d712bce97bb35c4dbb33637d4a6960d3836dd1d6
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-07-29 19:01:06 +0330

    chore: add Database URI

--- src/database/connectDB.ts
+++ src/database/connectDB.ts
@@ -1,19 +1,21 @@
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
 
-type ConnectOptions = {
+type MongooseOptionType = {
     useNewUrlParser: Boolean,
     useUnifiedTopology: Boolean,
 }
 
-const options: ConnectOptions = {
+const options: MongooseOptionType & ConnectOptions = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }
 
 const connectDB = async () => {
     try {
-        // await mongoose.connect()
+        await mongoose.connect(process.env.DATABASE_URI?.toString()!, options)
     } catch (error) {
-        
+        console.log(error)
     }
-}
+};
+
+export default connectDB;
