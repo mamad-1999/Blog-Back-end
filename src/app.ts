6c530ccf77a31728475064f2e7b7750cf47a81c8
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-07-29 18:08:26 +0330

    feat: add helmet middleware and config option

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express"
+import helmet from "helmet";
 
 require("dotenv").config();
 
 const app = express();
+
+app.use(helmet());
+app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
 
 app.use(express.json());
 
