ccef688e25f16fbc81d5885cc3c8db9e9275dc6c
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-07-29 19:05:02 +0330

    chore: add mongoose event When server started

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import app from './app';
+import mongoose from 'mongoose';
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Listening: http://localhost:${port}`);
-});
+mongoose.connection.once('open', () => {
+  console.log('CONNECTED TO MONGODB!')
+
+  app.listen(port, () => {
+    console.log(`Listening: http://localhost:${port}`);
+  });
+})
+
+mongoose.connection.on('error', (err) => {
+  console.log(err)
+})
