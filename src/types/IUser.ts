7bb702287d761233d5035b710997b529d8c5916a
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-07-31 18:26:51 +0330

    fix: add other type in User Type file

--- src/types/IUser.ts
+++ src/types/IUser.ts
@@ -1,13 +1,16 @@
+import { Types } from "mongoose"
+
 export type IUser = {
     email: string
     password: string
     name?: string
     image?: string
     phone?: string
-    biography: string
+    biography?: string
     role: "user" | "admin" | "superAdmin"
     birthdayDate?: string
     gender?: string
     twitterProfile?: string
     linkedinProfile?: string  
+    posts?: [Types.ObjectId]
 }
