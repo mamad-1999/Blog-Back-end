c979d525e43a07dffe369dad8c3c2ec320c53f8c
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-07-31 18:21:50 +0330

    chore: create initial User Type (not completed)

---
+++ src/types/IUser.ts
@@ -0,0 +1,13 @@
+export type IUser = {
+    email: string
+    password: string
+    name?: string
+    image?: string
+    phone?: string
+    biography: string
+    role: "user" | "admin" | "superAdmin"
+    birthdayDate?: string
+    gender?: string
+    twitterProfile?: string
+    linkedinProfile?: string  
+}
