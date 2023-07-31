7241529ac73e5df05d656797bf94d117f5c47783
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-07-31 18:57:18 +0330

    chore: create register filed validator function

---
+++ src/validators/register.ts
@@ -0,0 +1,14 @@
+import Validator from "fastest-validator";
+
+const v = new Validator()
+
+const schema = {
+    email: { type: 'email', normalize: true },
+    password: { type: 'string', trim: true, min: 8, max: 255 },
+    confirmPassword: { type: 'string', trim: true, min: 8, max: 255 },
+    $$strict: true
+}
+
+const check = v.compile(schema)
+
+export default check
