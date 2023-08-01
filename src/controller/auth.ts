2a325f41dbd44f4449e913e3a022692acd07d076
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-08-01 16:53:26 +0330

    chore: completed register controller

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -1,3 +1,47 @@
-export const register = async () => {
-    
+import { NextFunction, Request, Response } from "express";
+import checkValidation from '../validators/register';
+import errorController from "../utils/errorHandler";
+import bcrypt from 'bcrypt';
+import User from "../model/User";
+
+export const register = async (
+    req: Request,
+     res: Response,
+      next: NextFunction
+    ) => {
+    const { email, password, confirmPassword } = req.body;
+
+    if (!email || !password || !confirmPassword){
+        return res.status(400).json({ message: 'email, password and confirmPassword are required' })
+    }
+
+    try {
+        const checkData = await checkValidation({ email, password, confirmPassword })
+        if (checkData !== true){
+            errorController('Invalid Fields', 400, checkData);
+        }
+
+        if(password !== confirmPassword){
+            return res.status(400)
+                .json({ message: 'password and confirmPassword should be matched' })
+        }
+
+        const userDuplicate = await User.findOne({ email }).exec();
+        if (userDuplicate) return res.status(409)
+            .json({ message: 'This email already existed' });
+
+        const hashPassword = await bcrypt.hash(password, 12);
+
+        const result = await User.create({
+            email: email,
+            password: hashPassword
+        });
+
+        if (!result) {
+            errorController('user was not created... Please try again');
+        }
+        res.status(201).json({ message: 'New User Created :)', user: { email } })
+    } catch (error) {
+        next(error)
+    }
 }
