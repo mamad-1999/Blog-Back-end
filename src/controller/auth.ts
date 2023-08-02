47e163968884aacb6b350b64236f5f1366158c05
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-08-02 17:27:18 +0330

    feat: create login router and controller

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import bcrypt from 'bcrypt'
-import checkValidation from '../validators/register';
+import jwt from 'jsonwebtoken'
+import checkRegisterValidation from '../validators/register';
+import checkLoginValidation from '../validators/login';
 import errorHandler from "../utils/errorHandler";
 import User from "../model/User";
 
@@ -16,7 +18,7 @@ export const register = async (
     }
 
     try {
-        const checkData = await checkValidation({ email, password, confirmPassword })
+        const checkData = await checkRegisterValidation({ email, password, confirmPassword })
         if (checkData !== true){
             errorHandler('Invalid Fields', 400, checkData);
         }
@@ -41,6 +43,63 @@ export const register = async (
             errorHandler('user was not created... Please try again');
         }
         res.status(201).json({ message: 'New User Created :)', user: { email } })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const login = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const { email, password } = req.body;
+    if(!email || !password){
+        return res.status(400).json({ message: 'email and password are required' })
+    }
+
+    try {
+        const checkData = await checkLoginValidation({ email, password })
+        if(checkData !== true) {
+            errorHandler('Invalid Fields', 400, checkData)
+        }
+    
+        const foundUser = await User.findOne({ email }).exec();
+        if (!foundUser){
+            res.status(400).json({ message: "User with this email was not found!" })
+        }
+    
+        const match = await bcrypt.compare(password, foundUser!.password as string)
+        if(!match){
+            res.status(400).json({ message: 'Your password is wrong!' })
+        }
+    
+        const accessToken = jwt.sign(
+            { email: foundUser!.email, role: foundUser!.role },
+            process.env.ACCESS_TOKEN_SECRET?.toString()!,
+            { expiresIn: '16m' }
+        )
+    
+        const refreshToken = jwt.sign(
+            { email: foundUser!.email },
+            process.env.REFRESH_TOKEN_SECRET?.toString()!,
+            { expiresIn: '8d' }
+        )
+    
+        foundUser!.refreshToken = refreshToken
+        const result = await foundUser!.save()
+        if(!result){
+            errorHandler('User credential save was not Successfully... Try again')
+        }
+
+        res.cookie('jwt', refreshToken, {
+            sameSite: 'none',
+            httpOnly: true,
+            // secure: '',
+            maxAge: 8 * 24 * 60 * 60 * 1000 //8 day
+        })
+
+        res.status(200).json({ accessToken })
     } catch (error) {
         next(error)
     }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import { register } from '../controller/auth';
+import { register, login } from '../controller/auth';
 
 const route = express.Router()
 
 // Post => '/auth/register'
 route.post('/register', register)
 
-route.post('/login', )
+route.post('/login', login)
 
 export default route;
