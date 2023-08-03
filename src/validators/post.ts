c58b679968ac2c5969bdfd94f721ed5d966af436
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-08-03 16:35:11 +0330

    chore: create Post validator

---
+++ src/validators/post.ts
@@ -0,0 +1,14 @@
+import Validator from "fastest-validator";
+
+const v = new Validator()
+
+const schema = {
+    title: { type: 'string', max: 40, min: 3 },
+    description: { type: 'string', min: 10 },
+    image: { type: 'string', min: 10 },
+    $$strict: true,
+}
+
+const check = v.compile(schema)
+
+export default check
