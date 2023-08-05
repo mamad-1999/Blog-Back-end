284ab88af7f8a5799cd2676d041e2ecc25a55499
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-08-05 17:24:30 +0330

    feat: create delete Post endPoint

--- src/controller/post.ts
+++ src/controller/post.ts
@@ -4,6 +4,7 @@ import errorHandler from '../utils/errorHandler';
 import User from '../model/User';
 import Post from '../model/Post';
 import { IAddPost } from '../types/IPost';
+import mongoose from 'mongoose';
 
 export const createPost = async (
   req: Request<
@@ -36,6 +37,46 @@ export const createPost = async (
     await foundUser.save();
 
     res.status(201).json({ message: 'Post saved successfully', savePost });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deletePost = async (
+  req: Request<
+    { id: string },
+    Record<string, never>,
+    Record<string, never>,
+    Record<string, never>
+  >,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({ message: 'Invalid id' });
+  }
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+
+    const foundUser = await User.findOne({ _id: req.user })
+      .select('-password -refreshToken')
+      .exec();
+
+    if (!foundUser) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    if (post?.userId.toString() !== foundUser._id.toString()) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    await post.deleteOne();
+    await foundUser.updateOne({ $pull: { posts: req.params.id } });
+
+    res.status(200).json({ message: 'Post delete successfully', post });
   } catch (error) {
     next(error);
   }
--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-import { createPost } from '../controller/post';
+import { createPost, deletePost } from '../controller/post';
 import verifyJWT from '../middleware/verifyJWT';
 
 const route = express.Router();
 
 // POST => '/posts'
 route.post('/', verifyJWT, createPost);
+
+// DELETE => '/posts/:id'
+route.delete('/:id', verifyJWT, deletePost);
 
 export default route;
