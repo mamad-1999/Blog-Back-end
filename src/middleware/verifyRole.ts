911dd6055888917c5f27983051477234fa49ce94
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-08-08 21:54:35 +0330

    chore: remove log

--- src/middleware/verifyRole.ts
+++ src/middleware/verifyRole.ts
@@ -7,7 +7,6 @@ const verifyRole = (role: Role) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       const foundUser = await User.findById(req.user).lean().exec();
-      console.log(req.user);
       if (!foundUser) {
         return res.status(403).json({ message: 'Forbidden... User not found' });
       }
