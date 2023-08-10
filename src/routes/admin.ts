ecb30293d2f326f7332bd6c040b9792ffa095f50
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-08-10 19:23:32 +0330

    chore: create shared routes between users and admins

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -8,6 +8,13 @@ import {
   getAdmins,
   updateAdmin,
 } from '../controller/admin';
+import {
+  getReadingLists,
+  saveReadingList,
+  unSaveReadingList,
+} from '../controller/readingList';
+import { favorites, getFavorites, unFavorites } from '../controller/favorites';
+import { getPostsByUserId } from '../controller/user';
 
 const route = express.Router();
 
@@ -25,5 +32,26 @@ route.get('/', verifyJWT, verifyRole('superAdmin'), getAdmins);
 
 // GET => '/admins/:aid'
 route.get('/:aid', verifyJWT, verifyRole('admin'), getAdmin);
+
+// POST => '/admins/reading-list/:postId'
+route.post('/reading-list/:postId', verifyJWT, verifyRole('admin'), saveReadingList);
+
+// DELETE => '/admins/reading-list/:postId'
+route.delete('/reading-list/:postId', verifyJWT, verifyRole('admin'), unSaveReadingList);
+
+// GET => '/admins/reading-list/:uid'
+route.get('/reading-list/:uid', verifyJWT, verifyRole('admin'), getReadingLists);
+
+// POST => '/admins/favorites'
+route.post('/favorites', verifyJWT, verifyRole('admin'), favorites);
+
+// DELETE => '/admins/un-favorites'
+route.post('/un-favorites', verifyJWT, verifyRole('admin'), unFavorites);
+
+// GET => '/admins/favorites/:uid'
+route.get('/favorites/:uid', verifyJWT, verifyRole('admin'), getFavorites);
+
+// GET => '/admins/my-post/:uid'
+route.get('/my-post/:uid', verifyJWT, verifyRole('admin'), getPostsByUserId);
 
 export default route;
