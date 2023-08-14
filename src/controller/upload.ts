603e1629ad18f4ee7c15d361fc9f4923a5fc5caf
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-08-14 16:06:16 +0330

    fix: delete original image saved by multer

--- src/controller/upload.ts
+++ src/controller/upload.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import User from '../model/User';
 import sharp from 'sharp';
 import { uploadImage } from '../utils/multer';
+import fileDelete from '../utils/fileDeleter';
 
 export const uploadProfile = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -21,11 +22,16 @@ export const uploadProfile = async (req: Request, res: Response, next: NextFunct
             return res.status(404).json({ message: 'User not found' });
           }
           const fileUploadName = `${new Date().toISOString()}_${req.file.originalname}`;
-          await sharp(req.file.path)
+          sharp(req.file.path)
             .jpeg({ quality: 70 })
             .resize(300, 300)
-            .toFile(`src/uploads/sharp-${fileUploadName}`);
+            .toFile(`src/uploads/sharp-${fileUploadName}`, (err) => {
+              if (err) {
+                return res.status(400).json({ err });
+              }
 
+              fileDelete(req.file!.path);
+            });
           foundUser.image = `/src/uploads/sharp-${fileUploadName}`;
           await foundUser.save();
 
