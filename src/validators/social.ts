0c4b88898ab00da3ae24fcef074926a7999ffb0c
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-08-23 14:24:54 +0330

    chore: create social validator

---
+++ src/validators/social.ts
@@ -0,0 +1,14 @@
+import Validator from 'fastest-validator';
+
+const v = new Validator();
+
+const schema = {
+  username: { type: 'string', trim: true, min: 3, max: 255 },
+  email: { type: 'email', normalize: true },
+  profileUrl: { type: 'url' },
+  $$strict: true,
+};
+
+const check = v.compile(schema);
+
+export default check;
