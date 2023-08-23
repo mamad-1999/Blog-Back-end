e12b1c919b404b8567e103b619cd7dde00dc81fe
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-08-23 17:56:50 +0330

    fix: check path in deleteImage middleware

--- src/middleware/deleteImage.ts
+++ src/middleware/deleteImage.ts
@@ -2,7 +2,9 @@ import { NextFunction, Request, Response } from 'express';
 import fileDelete from '../utils/fileDeleter';
 
 const deleteImage = (req: Request, res: Response, next: NextFunction) => {
-  fileDelete(req.file!.path);
+  if (req.file?.path) {
+    fileDelete(req.file.path);
+  }
   next();
 };
 
